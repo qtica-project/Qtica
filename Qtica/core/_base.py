98dc53bf9df40017b1f5b92d094e0052882ad3e2
omamkaz <omamkaz@gmail.com>
2024-01-19 00:39:44 +0300

    fixed: repeating call of [add]Method when inserting to it Iterable value.

--- Qtica/core/_base.py
+++ Qtica/core/_base.py
@@ -1,13 +1,49 @@
 #!/usr/bin/python3
 
-from typing import Any, Sequence, Union
+from typing import Any, Callable, Sequence, Union
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt, QObject
-from ..utils._classes import Func, Args
 from ..enums.events import Events, EventTypeVar
 from ..enums.widgets import Widgets, WidgetTypeVar
 from ..enums.signals import Signals, SignalTypeVar
 from ..utils.caseconverter import camelcase, snakecase
+
+
+class Func:
+    def __init__(self, func, *args, **kwargs):
+        self._func = func
+        self._args = args
+        self._kwargs = kwargs
+
+    def func(self) -> Union[Callable, str]:
+        return self._func
+
+    def args(self) -> tuple[Any]:
+        return self._args
+
+    def kwargs(self) -> dict[str, Any]:
+        return self._kwargs
+
+
+class Args:
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def args(self) -> list[Any]:
+        return self._args
+
+    def kwargs(self) -> dict[str, Any]:
+        return self._kwargs
+
+
+class MArgs:
+    def __init__(self, *args: Union[Args, Any]):
+        self._args = args
+
+    def args(self) -> list[Union[Args, Any]]:
+        return self._args
+
 
 
 class AbstractBase:
@@ -134,8 +170,10 @@ class AbstractBase:
                                                     value.strip(), 
                                                     Qt.FindChildOption.FindChildrenRecursively)) is not None:
                     self.setParent(widget)
+
         elif callable(value):
             self.setParent(value())
+
         else:
             self.setParent(value)
 
@@ -148,8 +186,8 @@ class AbstractBase:
 
     def _handle_add_methods(self, name: str, value: Any) -> None:
         if (func := self._getattr(name)) is not None and callable(func):
-            if isinstance(value, (tuple, list, set)):
-                for v in value:
+            if isinstance(value, MArgs):
+                for v in value.args():
                     if isinstance(v, Args):
                         func(*v.args(), **v.kwargs())
                     else:
