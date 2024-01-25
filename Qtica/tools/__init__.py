1d1491adc50cd69954f39a6d7905a52eab79c793
omamkaz <omamkaz@gmail.com>
2024-01-26 02:50:30 +0300

    removed: ElidedText import

--- Qtica/tools/__init__.py
+++ Qtica/tools/__init__.py
@@ -9,8 +9,8 @@ from .qt_file_open import File
 from .settings import Settings
 from .ui_loader import UiLoader
 from .system_tray import SystemTray
-from .elided_text import ElidedText
 from .size_policy import SizePolicy
 from .smooth_scroll import SmoothScroll
 from .copy_progress import CopyProgress
 from .action import LinePasswordAction, MenuAction
+from .spacer_item import SpacerItem
