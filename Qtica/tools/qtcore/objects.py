866a0ccf58258db64cc3053696306462f93d5691
omamkaz <omamkaz@gmail.com>
2024-01-31 02:01:02 +0300

    updated: now can add *args to `__init__` method

--- Qtica/tools/qtcore/objects.py
+++ Qtica/tools/qtcore/objects.py
@@ -5,180 +5,180 @@ from ...core import AbstractQObject
 
 
 class AnimationGroup(AbstractQObject, QtCore.QAnimationGroup):
-    def __init__(self, **kwargs):
-        QtCore.QAnimationGroup.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QAnimationGroup.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Buffer(AbstractQObject, QtCore.QBuffer):
-    def __init__(self, **kwargs):
-        QtCore.QBuffer.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QBuffer.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class ConcatenateTablesProxyModel(AbstractQObject, QtCore.QConcatenateTablesProxyModel):
-    def __init__(self, **kwargs):
-        QtCore.QConcatenateTablesProxyModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QConcatenateTablesProxyModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class CoreApplication(AbstractQObject, QtCore.QCoreApplication):
-    def __init__(self, **kwargs):
-        QtCore.QCoreApplication.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QCoreApplication.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class EventLoop(AbstractQObject, QtCore.QEventLoop):
-    def __init__(self, **kwargs):
-        QtCore.QEventLoop.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QEventLoop.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class FileDevice(AbstractQObject, QtCore.QFileDevice):
-    def __init__(self, **kwargs):
-        QtCore.QFileDevice.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QFileDevice.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class FileSelector(AbstractQObject, QtCore.QFileSelector):
-    def __init__(self, **kwargs):
-        QtCore.QFileSelector.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QFileSelector.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class FileSystemWatcher(AbstractQObject, QtCore.QFileSystemWatcher):
-    def __init__(self, **kwargs):
-        QtCore.QFileSystemWatcher.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QFileSystemWatcher.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class IODevice(AbstractQObject, QtCore.QIODevice):
-    def __init__(self, **kwargs):
-        QtCore.QIODevice.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QIODevice.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class IdentityProxyModel(AbstractQObject, QtCore.QIdentityProxyModel):
-    def __init__(self, **kwargs):
-        QtCore.QIdentityProxyModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QIdentityProxyModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class ItemSelectionModel(AbstractQObject, QtCore.QItemSelectionModel):
-    def __init__(self, **kwargs):
-        QtCore.QItemSelectionModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QItemSelectionModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Library(AbstractQObject, QtCore.QLibrary):
-    def __init__(self, **kwargs):
-        QtCore.QLibrary.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLibrary.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class MimeData(AbstractQObject, QtCore.QMimeData):
-    def __init__(self, **kwargs):
-        QtCore.QMimeData.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QMimeData.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class PauseAnimation(AbstractQObject, QtCore.QPauseAnimation):
-    def __init__(self, **kwargs):
-        QtCore.QPauseAnimation.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QPauseAnimation.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class PluginLoader(AbstractQObject, QtCore.QPluginLoader):
-    def __init__(self, **kwargs):
-        QtCore.QPluginLoader.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QPluginLoader.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Process(AbstractQObject, QtCore.QProcess):
-    def __init__(self, **kwargs):
-        QtCore.QProcess.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QProcess.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SaveFile(AbstractQObject, QtCore.QSaveFile):
-    def __init__(self, **kwargs):
-        QtCore.QSaveFile.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSaveFile.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SharedMemory(AbstractQObject, QtCore.QSharedMemory):
-    def __init__(self, **kwargs):
-        QtCore.QSharedMemory.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSharedMemory.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SignalMapper(AbstractQObject, QtCore.QSignalMapper):
-    def __init__(self, **kwargs):
-        QtCore.QSignalMapper.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSignalMapper.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SocketNotifier(AbstractQObject, QtCore.QSocketNotifier):
-    def __init__(self, **kwargs):
-        QtCore.QSocketNotifier.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSocketNotifier.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SortFilterProxyModel(AbstractQObject, QtCore.QSortFilterProxyModel):
-    def __init__(self, **kwargs):
-        QtCore.QSortFilterProxyModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSortFilterProxyModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class StringListModel(AbstractQObject, QtCore.QStringListModel):
-    def __init__(self, **kwargs):
-        QtCore.QStringListModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QStringListModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class TemporaryFile(AbstractQObject, QtCore.QTemporaryFile):
-    def __init__(self, **kwargs):
-        QtCore.QTemporaryFile.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTemporaryFile.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Thread(AbstractQObject, QtCore.QThread):
-    def __init__(self, **kwargs):
-        QtCore.QThread.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QThread.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class ThreadPool(AbstractQObject, QtCore.QThreadPool):
-    def __init__(self, **kwargs):
-        QtCore.QThreadPool.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QThreadPool.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class TimeLine(AbstractQObject, QtCore.QTimeLine):
-    def __init__(self, **kwargs):
-        QtCore.QTimeLine.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTimeLine.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Timer(AbstractQObject, QtCore.QTimer):
-    def __init__(self, **kwargs):
-        QtCore.QTimer.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTimer.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Translator(AbstractQObject, QtCore.QTranslator):
-    def __init__(self, **kwargs):
-        QtCore.QTranslator.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTranslator.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class TransposeProxyModel(AbstractQObject, QtCore.QTransposeProxyModel):
-    def __init__(self, **kwargs):
-        QtCore.QTransposeProxyModel.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTransposeProxyModel.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class VariantAnimation(AbstractQObject, QtCore.QVariantAnimation):
-    def __init__(self, **kwargs):
-        QtCore.QVariantAnimation.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QVariantAnimation.__init__(self, *args)
         super().__init__(**kwargs)
--- Qtica/tools/qtcore/tools.py
+++ Qtica/tools/qtcore/tools.py
@@ -5,210 +5,210 @@ from ...core import AbstractTool
 
 
 class BitArray(AbstractTool, QtCore.QBitArray):
-    def __init__(self, **kwargs):
-        QtCore.QBitArray.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QBitArray.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class ByteArray(AbstractTool, QtCore.QByteArray):
-    def __init__(self, **kwargs):
-        QtCore.QByteArray.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QByteArray.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class ByteArrayMatcher(AbstractTool, QtCore.QByteArrayMatcher):
-    def __init__(self, **kwargs):
-        QtCore.QByteArrayMatcher.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QByteArrayMatcher.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Collator(AbstractTool, QtCore.QCollator):
-    def __init__(self, **kwargs):
-        QtCore.QCollator.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QCollator.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class DataStream(AbstractTool, QtCore.QDataStream):
-    def __init__(self, **kwargs):
-        QtCore.QDataStream.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QDataStream.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Date(AbstractTool, QtCore.QDate):
-    def __init__(self, **kwargs):
-        QtCore.QDate.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QDate.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class DateTime(AbstractTool, QtCore.QDateTime):
-    def __init__(self, **kwargs):
-        QtCore.QDateTime.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QDateTime.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class DeadlineTimer(AbstractTool, QtCore.QDeadlineTimer):
-    def __init__(self, **kwargs):
-        QtCore.QDeadlineTimer.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QDeadlineTimer.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Dir(AbstractTool, QtCore.QDir):
-    def __init__(self, **kwargs):
-        QtCore.QDir.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QDir.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class EasingCurve(AbstractTool, QtCore.QEasingCurve):
-    def __init__(self, **kwargs):
-        QtCore.QEasingCurve.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QEasingCurve.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class FileInfo(AbstractTool, QtCore.QFileInfo):
-    def __init__(self, **kwargs):
-        QtCore.QFileInfo.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QFileInfo.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class FutureInterfaceBase(AbstractTool, QtCore.QFutureInterfaceBase):
-    def __init__(self, **kwargs):
-        QtCore.QFutureInterfaceBase.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QFutureInterfaceBase.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class JsonDocument(AbstractTool, QtCore.QJsonDocument):
-    def __init__(self, **kwargs):
-        QtCore.QJsonDocument.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QJsonDocument.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Line(AbstractTool, QtCore.QLine):
-    def __init__(self, **kwargs):
-        QtCore.QLine.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLine.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class LineF(AbstractTool, QtCore.QLineF):
-    def __init__(self, **kwargs):
-        QtCore.QLineF.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLineF.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Locale(AbstractTool, QtCore.QLocale):
-    def __init__(self, **kwargs):
-        QtCore.QLocale.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLocale.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class LockFile(AbstractTool, QtCore.QLockFile):
-    def __init__(self, **kwargs):
-        QtCore.QLockFile.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLockFile.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class LoggingCategory(AbstractTool, QtCore.QLoggingCategory):
-    def __init__(self, **kwargs):
-        QtCore.QLoggingCategory.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QLoggingCategory.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Margins(AbstractTool, QtCore.QMargins):
-    def __init__(self, **kwargs):
-        QtCore.QMargins.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QMargins.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class MarginsF(AbstractTool, QtCore.QMarginsF):
-    def __init__(self, **kwargs):
-        QtCore.QMarginsF.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QMarginsF.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class NativeIpcKey(AbstractTool, QtCore.QNativeIpcKey):
-    def __init__(self, **kwargs):
-        QtCore.QNativeIpcKey.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QNativeIpcKey.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Point(AbstractTool, QtCore.QPoint):
-    def __init__(self, **kwargs):
-        QtCore.QPoint.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QPoint.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class PointF(AbstractTool, QtCore.QPointF):
-    def __init__(self, **kwargs):
-        QtCore.QPointF.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QPointF.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Rect(AbstractTool, QtCore.QRect):
-    def __init__(self, **kwargs):
-        QtCore.QRect.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QRect.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class RectF(AbstractTool, QtCore.QRectF):
-    def __init__(self, **kwargs):
-        QtCore.QRectF.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QRectF.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class RegularExpression(AbstractTool, QtCore.QRegularExpression):
-    def __init__(self, **kwargs):
-        QtCore.QRegularExpression.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QRegularExpression.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Size(AbstractTool, QtCore.QSize):
-    def __init__(self, **kwargs):
-        QtCore.QSize.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSize.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SizeF(AbstractTool, QtCore.QSizeF):
-    def __init__(self, **kwargs):
-        QtCore.QSizeF.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSizeF.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class SystemSemaphore(AbstractTool, QtCore.QSystemSemaphore):
-    def __init__(self, **kwargs):
-        QtCore.QSystemSemaphore.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QSystemSemaphore.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class TextStream(AbstractTool, QtCore.QTextStream):
-    def __init__(self, **kwargs):
-        QtCore.QTextStream.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTextStream.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Time(AbstractTool, QtCore.QTime):
-    def __init__(self, **kwargs):
-        QtCore.QTime.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QTime.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class Url(AbstractTool, QtCore.QUrl):
-    def __init__(self, **kwargs):
-        QtCore.QUrl.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QUrl.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class UrlQuery(AbstractTool, QtCore.QUrlQuery):
-    def __init__(self, **kwargs):
-        QtCore.QUrlQuery.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QUrlQuery.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class XmlStreamReader(AbstractTool, QtCore.QXmlStreamReader):
-    def __init__(self, **kwargs):
-        QtCore.QXmlStreamReader.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QXmlStreamReader.__init__(self, *args)
         super().__init__(**kwargs)
 
 
 class XmlStreamWriter(AbstractTool, QtCore.QXmlStreamWriter):
-    def __init__(self, **kwargs):
-        QtCore.QXmlStreamWriter.__init__(self)
+    def __init__(self, *args, **kwargs):
+        QtCore.QXmlStreamWriter.__init__(self, *args)
         super().__init__(**kwargs)
