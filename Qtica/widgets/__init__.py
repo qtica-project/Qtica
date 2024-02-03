2a9795fe9146886abb3a805525f8c1aba3564181
omamkaz <omamkaz@gmail.com>
2024-02-04 00:27:09 +0300

    added: WidgetContainer, FrameContainer

--- Qtica/widgets/__init__.py
+++ Qtica/widgets/__init__.py
@@ -7,14 +7,13 @@ from ._widgets import *
 from .menu import Menu
 from .label import Label
 from .stack import Stack
-from .widget import Widget
 from .scroll_area import ScrollArea
 from .icon_widget import IconWidget
 from .line import Line, HLine, VLine
 from .buttons import PushButton, ToolButton
 from .application import Application, App
 from .stacked_widget import StackedWidget
-from .frame import Container, Frame
+from .container import Container, FrameContainer, WidgetContainer
 from .size_grip import WindowSizeGrip
 from .group_box import GroupBox
 from .eliding_label import ElidingLabel
