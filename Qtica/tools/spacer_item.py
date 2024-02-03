2cf733ea2d3f16065ff8b19543056b7629843ab4
omamkaz <omamkaz@gmail.com>
2024-02-03 23:22:20 +0300

    changed: remove parameters name, added

--- Qtica/tools/spacer_item.py
+++ Qtica/tools/spacer_item.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from PySide6.QtWidgets import QSpacerItem, QSizePolicy
+from PySide6.QtWidgets import QSpacerItem
 from ..core import AbstractTool
 
 
 class SpacerItem(AbstractTool, QSpacerItem):
-    def __init__(self, 
-                 width: int, 
-                 height: int, 
-                 wPolicy: QSizePolicy.Policy = QSizePolicy.Policy.Minimum, 
-                 hPolicy: QSizePolicy.Policy = QSizePolicy.Policy.Minimum, 
-                 **kwargs):
-        QSpacerItem.__init__(self, width, height, wPolicy, hPolicy)
+    def __init__(self, *args, **kwargs):
+        QSpacerItem.__init__(self, *args)
         super().__init__(**kwargs)
