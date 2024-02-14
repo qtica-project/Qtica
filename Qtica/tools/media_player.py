a496f1798d582616fa9e5572706fdc8bc8dafdba
omamkaz <omamkaz@gmail.com>
2024-02-14 23:05:53 +0300

    added: MediaPlayer class

---
+++ Qtica/tools/media_player.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from ..core import AbstractQObject
+from PySide6.QtMultimedia import QMediaPlayer, QVideoSink
+
+
+class MediaPlayer(AbstractQObject, QMediaPlayer):
+    def __init__(self, running: bool = False, **kwargs):
+        QMediaPlayer.__init__(self)
+        super().__init__(**kwargs)
+
+        if (audio_out := kwargs.get("setAudioOutput", None)):
+            audio_out.setParent(self)
+
+        if (video_sink := kwargs.get("setVideoSink", None)):
+            video_sink.setParent(self)
+
+        if running:
+            self.play()
