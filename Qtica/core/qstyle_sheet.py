eb6fcc540bc1f835e0ab715c75f36cf26b1b21b7
omamkaz <omamkaz@gmail.com>
2024-02-19 21:36:58 +0300

    added: BorderRadius, RadialGradient, LinearGradient, BoxShadow classes to convert python values to Qss element

--- Qtica/core/qstyle_sheet.py
+++ Qtica/core/qstyle_sheet.py
@@ -2,9 +2,11 @@
 
 import os
 import json
+import math
 from string import Template
-from typing import Mapping, Union
-from PySide6.QtCore import QFile
+from typing import Mapping, Sequence, Union, Any
+from PySide6.QtCore import QFile, QPoint, QPointF
+from PySide6.QtGui import QColor, QRadialGradient, QLinearGradient
 
 
 class _QssTemplate(Template):
@@ -42,7 +44,88 @@ class QStyleSheet:
 
     '''
 
-    def __init__(self, 
+    class Element:
+        def __new__(cls, *args, **kwargs) -> Any:
+            instance = super().__new__(cls)
+            return instance.__init__(*args, **kwargs)
+
+    class BorderRadius(Element):
+        def __init__(self, 
+                     top_left: int = 0,
+                     top_right: int = 0,
+                     bottom_left: int = 0,
+                     bottom_right: int = 0) -> str:
+
+            return " ".join(map(lambda x: f"{x}px",
+                                (top_left, top_right, bottom_right, bottom_left)))
+
+    class RadialGradient(Element):
+        def __init__(self, 
+                     stops: Union[Sequence[tuple[float, Union[QColor, str]]], 
+                                  dict[float, Union[QColor, str]]],
+                     cx: float = None, 
+                     cy: float = None,
+                     fx: float = None,
+                     fy: float = None,
+                     radius: float = None,
+                     spread: Union[str, QRadialGradient.Spread] = None,
+                     coordinate: Union[str, QRadialGradient.CoordinateMode] = None
+                     ) -> str:
+
+            if isinstance(spread, QRadialGradient.Spread):
+                spread = spread.name.removesuffix("Spread").lower()
+
+            if isinstance(coordinate, QRadialGradient.CoordinateMode):
+                coordinate = coordinate.name.removesuffix("Mode")
+                coordinate[0] = coordinate[0].lower()
+
+            args = {"cx": cx, "cy": cy, "radius": radius, "fx": fx, "fy": fy, "spread": spread, "coordinateMode": coordinate}
+            data = [f"{k}: {v}" for k, v in args.items() if v is not None]
+            for value, color in (stops.items() if isinstance(stops, dict) else stops):
+                if isinstance(color, QColor):
+                    color = f"rgba({','.join(map(str, color.toTuple()))})"
+                if value > 1:
+                    value = value / 100
+                data.append(f"stop: {value} {color}")
+            return f"qradialgradient({', '.join(data)})"
+    
+    class LinearGradient(Element):
+        def __init__(self, 
+                     stops: Union[Sequence[tuple[float, Union[QColor, str]]], 
+                                  dict[float, Union[QColor, str]]],
+                     start: Union[QPoint, QPointF] = None, 
+                     end: Union[QPoint, QPointF] = None,
+                     degree: int = None) -> str:
+
+            data = []
+            for value, color in (stops.items() if isinstance(stops, dict) else stops):
+                if isinstance(color, QColor):
+                    color = f"rgba({','.join(map(str, color.toTuple()))})"
+                if value > 1:
+                    value = value / 100
+                data.append(f"stop: {value} {color}")
+
+            if degree is not None:
+                degree = f"x1: {math.cos(degree)}, y1: {math.sin(degree)}, x2: 1, y2: 1"
+            else:
+                x1, y1, x2, y2 = start.toTuple(), end.toTuple()
+                degree = f"x1: {x1}, y1: {y1}, x2: {x2}, y2: {y2}"
+
+            return f"qlineargradient({degree}, {', '.join(data)})"
+
+    class BoxShadow(Element):
+        def __init__(self, 
+                     x: float,
+                     y: float,
+                     blur_radius: float,
+                     color: Union[QColor, str]):
+
+            if isinstance(color, QColor):
+                color = f"rgba({','.join(map(str, color.toTuple()))})"
+
+            return f"{x}px {y}px {blur_radius}px 0px {color}"
+
+    def __init__(self,
                  qss: Union[dict, str],
                  vars: dict = None):
 
