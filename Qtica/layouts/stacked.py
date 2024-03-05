df4a8533a1f9f71e14fe09aa5477f88d254e4278
omamkaz <omamkaz@gmail.com>
2024-03-06 00:52:32 +0300

    added: Routes, dict types support

--- Qtica/layouts/stacked.py
+++ Qtica/layouts/stacked.py
@@ -1,13 +1,13 @@
 from typing import Union
 from PySide6.QtWidgets import QStackedLayout, QWidget, QLayoutItem
 from ..utils.alignment import Alignment
-from ..core import AbstractQObject
+from ..core import AbstractQObject, Routes
 
 
 class StackedLayout(AbstractQObject, QStackedLayout):
     def __init__(self,
                  *,
-                 children: list[Union[QWidget, QLayoutItem, Alignment]] = None,
+                 children: Union[list[Union[QWidget, QLayoutItem, Alignment]], Routes, dict] = None,
                  **kwargs):
         QStackedLayout.__init__(self)
         super().__init__(**kwargs)
@@ -15,20 +15,26 @@ class StackedLayout(AbstractQObject, QStackedLayout):
         if not children:
             return
 
-        for child in children:
-            if isinstance(child, Alignment):
-                _widget = child.child
+        if isinstance(children, (Routes, dict)):
+            self.routes = Routes("/", **children) if isinstance(children, dict) else children
+            self.routes._set_stacked(self)
+            for route, child in children.items():
+                self.routes.add(route, child)
+        else:
+            for child in children:
+                if isinstance(child, Alignment):
+                    _widget = child.child
 
-                if isinstance(_widget, QWidget):
-                    _func = self.addWidget
-                elif isinstance(_widget, QLayoutItem):
-                    _func = self.addItem
+                    if isinstance(_widget, QWidget):
+                        _func = self.addWidget
+                    elif isinstance(_widget, QLayoutItem):
+                        _func = self.addItem
 
-                _func(_widget)
-                self.setAlignment(_widget, child.alignment)
+                    _func(_widget)
+                    self.setAlignment(_widget, child.alignment)
 
-            elif isinstance(child, QLayoutItem):
-                self.addItem(child)
+                elif isinstance(child, QLayoutItem):
+                    self.addItem(child)
 
-            elif isinstance(child, QWidget):
-                self.addWidget(child)
+                elif isinstance(child, QWidget):
+                    self.addWidget(child)
