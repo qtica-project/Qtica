7f1a4787f5da168172b102e1674a4db6668add43
omamkaz <omamkaz@gmail.com>
2024-03-22 01:19:18 +0300

    added: PosEventsRange, PosEventsArg, PosEvents classes

--- Qtica/core/_widget.py
+++ Qtica/core/_widget.py
@@ -1,11 +1,61 @@
 #!/usr/bin/python3
 
-from typing import Union
+from dataclasses import dataclass
+from typing import Callable, TypeAlias, Union
 from PySide6.QtWidgets import QGraphicsEffect
-from PySide6.QtCore import QTimer, Qt, Signal
+from PySide6.QtCore import QPoint, QPointF, QTimer, Qt, Signal
 from .qstyle_sheet import QStyleSheet
 from ._declarative import AbstractDec
 from ._base import AbstractBase
+
+
+@dataclass
+class PosEventsRange():
+    start: int 
+    stop: int
+
+    def _start(self) -> int:
+        if callable(self.start):
+            return self.start()
+        return self.start
+
+    def _stop(self) -> int:
+        if callable(self.stop):
+            return self.stop()
+        return self.stop
+
+
+@dataclass
+class PosEventsArg():
+    x: int | PosEventsRange
+    y: int | PosEventsRange
+    callback: Callable
+
+    def _check_var_type(self, _type: object) -> bool:
+        return all(isinstance(var, _type) for var in (self.x, self.y))
+
+    def _x_in_range(self, value: int) -> bool:
+        return self.x._stop() >= value >= self.x._start()
+
+    def _y_in_range(self, value: int) -> bool:
+        return self.y._stop() >= value >= self.y._start()
+
+    def _pos_in_range(self, x: int, y: int) -> bool:
+        return self._x_in_range(x) and self._y_in_range(y)
+
+    def _equal_pos(self, x: int, y: int) -> bool:
+        return self.x == x and self.y == y
+
+
+PosEventsTypeVar: TypeAlias = Union[tuple[int, int, Callable],
+                                    tuple[Union[QPoint, QPointF], Callable],
+                                    tuple[PosEventsRange, PosEventsRange, Callable],
+                                    PosEventsArg]
+@dataclass(kw_only=True)
+class PosEvents():
+    clicked: list[PosEventsTypeVar] = None
+    double_clicked: list[PosEventsTypeVar] = None
+    hover: list[PosEventsTypeVar] = None
 
 
 class AbstractWidget(AbstractBase):
@@ -17,8 +67,10 @@ class AbstractWidget(AbstractBase):
                  qss: Union[str, dict, QStyleSheet] = None,
                  attrs: Union[list[Qt.WidgetAttribute], dict[Qt.WidgetAttribute, bool]] = None,
                  flags: Union[list[Qt.WindowType], dict[Qt.WindowType, bool]] = None,
+                 at_pos: PosEvents = None,
                  **kwargs):
 
+        self._at_pos: PosEvents = at_pos
         self.__long_press_delay: int = long_press_delay
 
         self.__long_press_timer = QTimer(self)
@@ -76,9 +128,43 @@ class AbstractWidget(AbstractBase):
         self.__long_press_timer.stop()
         self.long_pressed.emit()
 
-    def mousePressEvent(self, event):
+    def _on_pos_events_arg(self, pos_event, x, y):
+        if pos_event._check_var_type(PosEventsRange) and pos_event._pos_in_range(x, y):
+            pos_event.callback()
+        elif pos_event._check_var_type(int) and pos_event._equal_pos(x, y):
+            pos_event.callback()
+
+    def _on_at_pos(self, at_pos, event):
+        for _pos_event in at_pos:
+            if isinstance(_pos_event, PosEventsArg):
+                pass
+            elif len(_pos_event) == 3:
+                _pos_event = PosEventsArg(*_pos_event)
+            elif len(_pos_event) == 2:
+                _pos_event = PosEventsArg(*map(int, _pos_event[0].toTuple()), _pos_event[-1])
+
+            if _pos_event is not None:
+                self._on_pos_events_arg(_pos_event, *event.position().toPoint().toTuple())
+
+    def mousePressEvent(self, event) -> None:
         self.__long_press_timer.start(self.__long_press_delay)
+
+        if self._at_pos is not None and self._at_pos.clicked is not None:
+            self._on_at_pos(self._at_pos.clicked, event)
+
         return super().mousePressEvent(event)
+
+    def mouseDoubleClickEvent(self, event) -> None:
+        if self._at_pos is not None and self._at_pos.double_clicked is not None:
+            self._on_at_pos(self._at_pos.double_clicked, event)
+
+        return super().mouseDoubleClickEvent(event)
+
+    def mouseMoveEvent(self, event) -> None:
+        if self._at_pos is not None and self._at_pos.hover is not None:
+            self._on_at_pos(self._at_pos.hover, event)
+
+        return super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event):
         self.__long_press_timer.stop()
