9b82218a26dc9171a260c4399fdd269ef6363ffe
omamkaz <omamkaz@gmail.com>
2024-05-24 22:16:38 +0300

    changed: it's now subclass of VHLayout

--- Qtica/layouts/h_layout.py
+++ Qtica/layouts/h_layout.py
@@ -1,58 +1,21 @@
 from typing import Union
-from PySide6.QtWidgets import QHBoxLayout, QLayoutItem, QSpacerItem, QWidget, QLayout
-from ..tools.wrappers.h_layout import HLayoutWrapper, ColumnLayoutWrapper
+from PySide6.QtWidgets import QHBoxLayout, QWidget, QLayout
+from ..tools.wrappers.layout import LayoutWrapper
 from ..utils.alignment import Alignment
-from ..core import AbstractQObject
+from .vh_layout import VHLayout
 
 
-class HLayout(AbstractQObject, QHBoxLayout):
+
+class HLayout(VHLayout, QHBoxLayout):
     def __init__(self,
                  *,
                  children: list[Union[QWidget, 
                                       QLayout, 
-                                      HLayoutWrapper,
-                                      ColumnLayoutWrapper,
+                                      LayoutWrapper,
                                       Alignment]] = None,
                  **kwargs):
         QHBoxLayout.__init__(self)
-        super().__init__(**kwargs)
-
-        if not children:
-            return
-
-        for child in children:
-            if isinstance(child, Alignment):
-                _widget = child.child
-
-                if isinstance(_widget, QWidget):
-                    _func = self.addWidget
-                elif isinstance(_widget, QLayoutItem):
-                    _func = self.addItem
-
-                _func(_widget)
-                self.setAlignment(_widget, child.alignment)
-
-            elif isinstance(child, HLayoutWrapper):
-                _widget = child.child
-
-                if isinstance(_widget, QWidget):
-                    _func = self.addWidget
-                elif isinstance(_widget, QLayout):
-                    _func = self.addLayout
-
-                _func(*child._yield_attr())
-
-            elif isinstance(child, QSpacerItem):
-                self.addSpacerItem(child)
-
-            elif isinstance(child, QLayoutItem):
-                self.addItem(child)
-
-            elif isinstance(child, QWidget):
-                self.addWidget(child)
-
-            elif isinstance(child, QLayout):
-                self.addLayout(child)
+        super().__init__(children=children, **kwargs)
 
 
 class ColumnLayout(HLayout):
