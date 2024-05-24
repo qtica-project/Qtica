d2c854f754274fca9ad2728a3f0a88fd88572094
omamkaz <omamkaz@gmail.com>
2024-05-24 22:21:18 +0300

    added: some classes!

--- Qtica/utils/__init__.py
+++ Qtica/utils/__init__.py
@@ -1,5 +1,15 @@
-from ._classes import *
 from ._env_var import EnvVar
 from ._text_wrap import TextWrap
-from .modifiers import Modifiers
 from .alignment import Alignment
+from .key_events import (
+    Modifiers,
+    Keys,
+    KeySequence
+)
+from ._classes import (
+    staticproperty,
+    classproperty,
+    QueryDict,
+    EnumDirectValueMeta,
+    BaseDir
+)
