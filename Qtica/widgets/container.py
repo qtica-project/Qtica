d04caa126d9282a2422bf53168f95baad124cb42
omamkaz <omamkaz@gmail.com>
2024-05-30 21:33:08 +0300

    updated: improve some code

--- Qtica/widgets/container.py
+++ Qtica/widgets/container.py
@@ -1,44 +1,37 @@
-from typing import Tuple, Union
+from typing import Union
 from PySide6.QtWidgets import QFrame, QLayout, QWidget
-from PySide6.QtCore import QMargins
 from ..core import AbstractWidget
 
 
-class FrameContainer(AbstractWidget, QFrame):
-    def __init__(self, 
-                 *,
-                 child: Union[QWidget, QLayout] = None,
-                 padding: Union[QMargins, Tuple[int, int, int, int]] = None,
-                 **kwargs):
+class AbstractContainer(AbstractWidget):
+    def __init__(self, child: Union[QWidget, QLayout, list[QWidget]] = None, **kwargs):
+        super().__init__(**kwargs)
+
+        if child is not None:
+            if isinstance(child, QWidget):
+                child.setParent(self)
+            elif isinstance(child, QLayout):
+                child.setProperty("parent", self)
+                self.setLayout(child)
+            elif isinstance(child, (list, tuple, set)):
+                for sub in child:
+                    sub.setParent(self)
+
+
+class FrameContainer(AbstractContainer, QFrame):
+    def __init__(self, *, child: Union[QWidget, QLayout, list[QWidget]] = None, **kwargs):
         QFrame.__init__(self)
 
         self.setFrameShape(QFrame.Shape.NoFrame)
         self.setFrameShadow(QFrame.Shadow.Raised)
 
-        super().__init__(**kwargs)
-
-        if child is not None:
-            if isinstance(child, QWidget):
-                child.setParent(self)
-            elif isinstance(child, QLayout):
-                child.setProperty("parent", self)
-                self.setLayout(child)
-
-        if padding is not None:
-            self.setContentsMargins(*padding if isinstance(padding, (tuple, list)) else padding)
+        super().__init__(child, **kwargs)
 
 
-class WidgetContainer(AbstractWidget, QWidget):
-    def __init__(self, *, child: Union[QWidget, QLayout] = None, **kwargs):
+class WidgetContainer(AbstractContainer, QWidget):
+    def __init__(self, *, child: Union[QWidget, QLayout, list[QWidget]] = None, **kwargs):
         QWidget.__init__(self)
-        super().__init__(**kwargs)
-
-        if child is not None:
-            if isinstance(child, QWidget):
-                child.setParent(self)
-            elif isinstance(child, QLayout):
-                child.setProperty("parent", self)
-                self.setLayout(child)
+        super().__init__(child, **kwargs)
 
 
 class Container(FrameContainer):
