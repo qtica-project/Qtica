b11a1ae0b4f8c5bfa0dac8a909e74315c8826de9
omamkaz <omamkaz@gmail.com>
2024-06-06 02:37:34 +0300

    added: AbstractIODevice class

--- Qtica/tools/qt_file_open.py
+++ Qtica/tools/qt_file_open.py
@@ -1,11 +1,11 @@
 from PySide6.QtCore import QFile, QTemporaryFile
+from PySide6.QtCore import QIODevice
+from ..core import AbstractQObject
 
 
-class OpenFile(QFile):
-    def __init__(self, 
-                 file: str,
-                 mode: QFile.OpenModeFlag = QFile.OpenModeFlag.ReadOnly):
-        super().__init__(file)
+class AbstractIODevice(AbstractQObject):
+    def __init__(self, mode: QIODevice.OpenModeFlag, **kwargs):
+        super().__init__(**kwargs)
         self._mode = mode
 
     def __enter__(self):
@@ -22,29 +22,24 @@ class OpenFile(QFile):
         return self.read_with_utf_8()
 
 
+class OpenFile(AbstractIODevice, QFile):
+    def __init__(self, 
+                 file: str,
+                 mode: QFile.OpenModeFlag = QFile.OpenModeFlag.ReadOnly,
+                 **kwargs):
+        QFile.__init__(self, file)
+        super().__init__(mode, **kwargs)
 
-class TempFile(QTemporaryFile):
+
+class TempFile(AbstractIODevice, QTemporaryFile):
     def __init__(self, 
                  mode: QTemporaryFile.OpenModeFlag = QTemporaryFile.OpenModeFlag.WriteOnly,
                  name: str = None,
-                 *args, 
                  **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-
-        self._mode = mode
-        if name is not None:
-            self.setFileName(name)
-    
-    def __enter__(self):
-        self.open(self._mode)
-        return self
+        QTemporaryFile.__init__(self, name)
+        super().__init__(mode, **kwargs)
 
     def __exit__(self, exception_type, exception_value, exception_traceback) -> None:
         self.close()
-        self.remove()
-
-    def read_with_utf_8(self) -> str:
-        return str(self.readAll(), encoding='utf-8')
-
-    def readUtf8(self) -> str:
-        return self.read_with_utf_8()
+        if self.autoRemove():
+            self.remove()
