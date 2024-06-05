98f388275cd9c99372d18a10dd6551f38c265759
omamkaz <omamkaz@gmail.com>
2024-06-05 17:15:37 +0300

    removed: **tool_bar**, **dock_widgets**

--- Qtica/tools/wrappers/__init__.py
+++ Qtica/tools/wrappers/__init__.py
@@ -1,7 +1,5 @@
 from .menu import MenuSeparatorWrapper, MenuSectionWrapper
-from .tool_bar import ToolBarBreakWrapper, ToolBarWrapper
 from .layout import LayoutWrapper
 from .border_layout import BorderLayoutWrapper
-from .dock_widgets import DockWidgetWrapper
 from .grid_layout import GridLayoutWrapper
 from .form_layout import FormLayoutWrapper
